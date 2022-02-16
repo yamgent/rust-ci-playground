e54aaea2e095e440b7ba36fa5641aadbd42ee860
Tan Wang Leng <tanwangleng@outlook.com>
2022-02-16 22:37:42 +0800

    Add faulty add function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+fn add(a: i32, b: i32) -> i32 {
+    a - b
+}
+
 fn main() {
     println!("Hello, world!");
+    println!("2 + 3 = {}", add(2, 3));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2,), 3);
+    }
 }
